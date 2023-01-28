50721ef7531c90bb33f24c8541f6a7b71a2747dc
Bunnitown <yskcooderz@gmail.com>
2023-01-27 21:06:50 -0300

    Arrumando erros e estilos

--- scripts.js
+++ scripts.js
@@ -1,38 +1,83 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
-const fetchData = () => {
+window.addEventListener("scroll", () => {
+    var pageYOffset = 384 + window.pageYOffset;
+    var headerOpacity = "rgba(0, 0, 0, 0." + pageYOffset + ")";
+    $("div.header").css("backgroundColor", headerOpacity);
+});
+
+const repeatElements = () => {
+    const elements = document.querySelectorAll('[times]');
+    if (elements) {
+        for (var i = 0; i < elements.length; i++) {
+            var c = elements[i].getAttribute("class");
+            var t = elements[i].getAttribute("times");
+            var p = elements[i].getAttribute("path");
+            var local = document.getElementsByName(p);
+            if (t && p && c && local.length > 0) {
+                for (n = 0; n < t; n++) {
+                    var panel = elements[i].cloneNode(true);
+                    local[i].append(panel);
+                }
+            }
+        }
+    }
+}
+
+const loadDatabase = () => {
+    const fetchDatabase = (c, s) => {
+        var images = c.querySelectorAll('.content .img'),
+        titles = c.querySelectorAll('.content .title'),
+        subtitles = c.querySelectorAll('.content .subtitle');
+    
+        const addCard = (n, t = '', s = '', i) => {
+            titles[n].innerHTML = t;
+            subtitles[n].innerHTML = s;
+    
+            const path = "./assets/musics/" + i;
+            $.ajax({
+                url: path,
+                type: 'HEAD',
+                success: () => images[n].style.backgroundImage = "url('" + path + "')"
+            });
+        }
+    
+        for (var i = 0; i < titles.length; i++) {
+            if (i == 0) addCard(i, "Peaceful Piano", "Relax and indulge with beautiful piano pieces", "ab67706f00000002ca5a7517156021292e5663a6.jpeg");
+            else if (i == 1) addCard(i, "Deep Focus", "Keep calm and focus with ambient and post-rock music.", "ab67706f000000025551996f500ba876bda73fa5.jpeg");
+            else addCard(i);
+        }
+    }
+
+    var path = "body .main .component";
+    var components = document.querySelectorAll(path);
+    if (components) {
+        for (var i = 0; i < components.length; i++) {
+            var name = components[i].getAttribute("name");
+            var search = components[i].getAttribute("search");
+            components[i].getElementsByClassName("title")[0].innerHTML = name;
+            fetchDatabase(components[i], search);
+        }
+    }
+}
+
+const fetchData = async () => {
     $(".header").load("./components/header.html");
     $(".navigator").load("./components/navigator.html");
-    $(".highlight").load("./components/highlight.html");
-}
+    $("body .main .component").load("./components/component.html");
 
-const fetchHighlight = () => {
-    var images = document.querySelectorAll('.highlight .content .img'),
-    titles = document.querySelectorAll('.highlight .content .title'),
-    subtitles = document.querySelectorAll('.highlight .content .subtitle');
+    $("html").hover(
+        () => $("html").css("overflowY", "overlay"),
+        () => $("html").css("overflowY", "hidden")
+    );
 
-    const addCard = (n, t = '', s = '', i) => {
-        titles[n].innerHTML = t;
-        subtitles[n].innerHTML = s;
+    $(".navigator").hover(
+        () => $("html").css("overflowY", "hidden"),
+        () => $("html").css("overflowY", "overlay")
+    );
 
-        const path = "./assets/" + i;
-        $.ajax({
-            url: path,
-            type: 'HEAD',
-            success: () => images[n].style.backgroundImage = "url('" + path + "')"
-        });
-    }
-
-    for (var i = 0; i < titles.length; i++) {
-        if (i == 0) addCard(i, "Peaceful Piano", "Relax and indulge with beautiful piano pieces", "icon.png");
-        else if (i == 1) addCard(i, "Deep Focus", "Keep calm and focus with ambient and post-rock music.");
-        else addCard(i);
-    }
-}
-
-const repeat = (times, content, path) => {
-    var panel = $(content).clone();
-    for (i = 0; i < times; i++) {
-        var new_panel = panel.clone();
-        $(path).append(new_panel);
-    }
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    await delay(20);
+    
+    repeatElements();
+    loadDatabase();
 }
